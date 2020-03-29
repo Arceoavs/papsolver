7137bd1bc6ef164abd099665a4ad6dc8aa9afeb3
Arnold Arz <arnoarz99@gmail.com>
2020-03-29 18:05:25 +0200

    :heavy_plus_sign: adding axios

--- papsolvue/src/main.js
+++ papsolvue/src/main.js
@@ -4,8 +4,10 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import API from "./services/http";
 
 Vue.config.productionTip = false;
+Vue.prototype.$http = API;
 
 new Vue({
   router,
