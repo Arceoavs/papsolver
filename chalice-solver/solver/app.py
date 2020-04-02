bc428596e9cccb7ca0d2bd6ccd0c9491a0c5b592
Arnold Arz <arnoarz99@gmail.com>
2020-04-02 14:40:36 +0200

    :sparkles: working iTunes balance

--- chalice-solver/solver/app.py
+++ chalice-solver/solver/app.py
@@ -14,7 +14,8 @@ import os
 app = Chalice(app_name="solver")
 
 # Configure CORS Routes
-cors_allow_origin = os.getenv("CORS_ALLOW_ORIGIN", "*")
+# cors_allow_origin = os.getenv("CORS_ALLOW_ORIGIN", "*")
+cors_allow_origin = "*"
 cors_config = CORSConfig(allow_origin=cors_allow_origin)
 
 # Configure debug output
@@ -40,12 +41,11 @@ class PrepaidSolver:
         objective.SetMaximization()
 
         for tier in self.tiers:
-            if not tier["active"]:
-                break
-            else:
-                var = self.solver.IntVar(0.0, self.solver.infinity(), tier["desc"])
-                constraint.SetCoefficient(var, tier["price"])
-                objective.SetCoefficient(var, tier["price"])
+            price = tier["price"]["int"]
+            var = self.solver.IntVar(0.0, self.solver.infinity(), tier["desc"])
+            
+            constraint.SetCoefficient(var, price)
+            objective.SetCoefficient(var, price)
 
     def formatSolution(self):
         max_object_value = int(self.solver.Objective().Value())
@@ -64,7 +64,7 @@ class PrepaidSolver:
         return self.formatSolution()
 
 
-@app.route("/", api_key_required=True)
+@app.route("/", methods=["GET"], api_key_required=True, cors=cors_config)
 def status():
     title = "PapSolver"
     version = "v1.0"
@@ -72,7 +72,7 @@ def status():
     return {"title": title, "version": version, "description": description}
 
 
-@app.route("/solve", methods=["POST"], api_key_required=True)
+@app.route("/solve", methods=["POST"], api_key_required=True, cors=True)
 def solve():
     tiers = app.current_request.json_body["tiers"]
     target = app.current_request.json_body["target"]
