d98a309bd11c1ac99fbf60cd7c2aca5c0dfb5f69
Arnold Arz <arnoarz99@gmail.com>
2020-04-02 14:57:39 +0200

    :lock: adding api key

--- papsolvue/src/services/http.js
+++ papsolvue/src/services/http.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+axios.defaults.headers.common["x-api-key"] =
+  process.env.VUE_APP_API_KEY || null;
+
 export default axios.create({
   baseURL: process.env.VUE_APP_API || ""
 });
